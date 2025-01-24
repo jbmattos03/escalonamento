ec8d22739ca60a53c9a694ead7a0be9875aa019c
thiagodias19 <thiagodiasfla16@gmail.com>
2025-01-24 20:59:08 -0300

    lru

--- src/base/memoria.py
+++ src/base/memoria.py
@@ -26,3 +26,25 @@ def subs_pag_fifo(lista_subs, num_paginas):
     return memoria
 
 print("Última página:",subs_pag_fifo(lista_subs, num_paginas))
+
+def lru(lista_subs, num_paginas):
+    memoria = []
+    lista = lista_subs[:]
+    reference = lista_subs[:]
+    pagina = 0
+    for page in lista:
+        if page in memoria:
+            memoria.remove(page)
+            memoria.append(page)
+            pagina += 1
+        else: 
+            if len(memoria) >= num_paginas :
+                memoria.pop(0)
+            memoria.append(page)
+            pagina += 1
+    
+        print(f"Páginas {pagina}: {memoria}")
+
+    return memoria
+
+print("Última página:", lru(lista_subs, num_paginas))
